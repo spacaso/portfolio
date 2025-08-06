5435264bc564783cfdf4d03dce5a8957ce1b5d28
Spacaso <avishyantaryan@gmail.com>
2025-08-06 10:22:01 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
-function openContactModal() {
-  document.getElementById("contact-modal").style.display = "flex";
+function openContact() {
+  document.getElementById("contactModal").style.display = "block";
 }
 
-function closeContactModal() {
-  document.getElementById("contact-modal").style.display = "none";
+function closeContact() {
+  document.getElementById("contactModal").style.display = "none";
 }
+
+window.onclick = function (event) {
+  const modal = document.getElementById("contactModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
